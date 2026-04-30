88632ef769cc879bcb9f19ce4d616a33086e4eb9
Ayrton Silva <ayrtonricardo2000@hotmail.com>
2026-04-29 21:46:39 -0300

    added: include summoner and league in match query

--- backend/src/repositories/riotMatches.repositories.ts
+++ backend/src/repositories/riotMatches.repositories.ts
@@ -6,70 +6,68 @@ import { createSummoner } from './riotAccount.repositories'
 export async function createMatches(request: RiotMatchesRequest) {
   const matches = await getMatches(request)
 
-
   for (const match of matches) {
     const doesMatchExist = await prisma.match.findUnique({
-            where: { matchId: match }
+      where: { matchId: match },
     })
-    if(!doesMatchExist){
-        
-    const matchData = await getMatchDetails({ matchId: match })
+    if (!doesMatchExist) {
+      const matchData = await getMatchDetails({ matchId: match })
 
-    try {
-      await prisma.match.create({
-        data: {
-          matchId: match,
-          gameDuration: matchData.info.gameDuration,
-          gameEndTimestamp: matchData.info.gameEndTimestamp.toString(),
-          gameMode: matchData.info.gameMode,
-          gameType: matchData.info.gameType
-        },
-      })
-    } catch (e) {
-      console.log(e)
-    }
+      try {
+        await prisma.match.create({
+          data: {
+            matchId: match,
+            gameDuration: matchData.info.gameDuration,
+            gameEndTimestamp: matchData.info.gameEndTimestamp.toString(),
+            gameMode: matchData.info.gameMode,
+            gameType: matchData.info.gameType,
+          },
+        })
+      } catch (e) {
+        console.log(e)
+      }
 
-        for (const participant of matchData.info.participants) {
+      for (const participant of matchData.info.participants) {
         try {
-            await createSummoner({
+          await createSummoner({
             gameName: participant.riotIdGameName,
             tagLine: participant.riotIdTagline,
-            })
+          })
 
-            await prisma.participant.upsert({
+          await prisma.participant.upsert({
             where: {
-                participantId: { summonerId: participant.puuid, matchId: match },
+              participantId: { summonerId: participant.puuid, matchId: match },
             },
             update: {},
             create: {
-                summonerId: participant.puuid,
-                matchId: match,
-                championName: participant.championName,
-                lane: participant.lane,
-                teamPosition: participant.teamPosition,
-                summoner1Id: participant.summoner1Id,
-                summoner2Id: participant.summoner2Id,
-                totalMinionsKilled: participant.totalMinionsKilled,
-                totalDamageDealtToChampions:
+              summonerId: participant.puuid,
+              matchId: match,
+              championName: participant.championName,
+              lane: participant.lane,
+              teamPosition: participant.teamPosition,
+              summoner1Id: participant.summoner1Id,
+              summoner2Id: participant.summoner2Id,
+              totalMinionsKilled: participant.totalMinionsKilled,
+              totalDamageDealtToChampions:
                 participant.totalDamageDealtToChampions,
-                wardsPlaced: participant.wardsPlaced,
-                goldEarned: participant.goldEarned,
-                item0: participant.item0,
-                item1: participant.item1,
-                item2: participant.item2,
-                item3: participant.item3,
-                item4: participant.item4,
-                item5: participant.item5,
-                item6: participant.item6,
-                win: participant.win
+              wardsPlaced: participant.wardsPlaced,
+              goldEarned: participant.goldEarned,
+              item0: participant.item0,
+              item1: participant.item1,
+              item2: participant.item2,
+              item3: participant.item3,
+              item4: participant.item4,
+              item5: participant.item5,
+              item6: participant.item6,
+              win: participant.win,
             },
-            })
+          })
         } catch (e) {
-            console.log(e)
+          console.log(e)
         }
-        }
+      }
     }
-    }
+  }
 
   return matches
 }
@@ -79,7 +77,15 @@ export async function readMatch(id: string) {
     const match = await prisma.match.findUnique({
       where: { matchId: id },
       include: {
-        participants: true,
+        participants: {
+          include: {
+            summoner: {
+              include: {
+                summonerLeagues: true,
+              },
+            },
+          },
+        },
       },
     })
 
